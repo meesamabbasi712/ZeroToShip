dfffbe590698672cd93e7ba2155486f4459350a7
meesamabbasi712 <meesamabbasi783@gmail.com>
2026-07-21 12:46:16 +0500

    Add config.py with file categorization thresholds

---
+++ Phase-1/config.py
@@ -0,0 +1,15 @@
+"""
+DoomFolder
+"""
+LARGE_FILE_SIZE_MB = 100
+OLD_FILE_DAYS = 365
+SCREENSHOT_EXTENSIONS = [
+    ".png",
+    ".jpg",
+    ".jpeg"
+]
+ZIP_EXTENSIONS = [
+    ".zip",
+    ".rar",
+    ".7z"
+]
